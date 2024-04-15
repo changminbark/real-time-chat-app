9e00accdaf252b1124d216293406b00ffa09ffa1
Chang Min Bark <changminbark@gmail.com>
2024-04-14 22:49:34 -0400

    created frontend login page

---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,34 @@
+import { VStack, ButtonGroup, FormControl, FormLabel, Button, FormErrorMessage, Input, Heading } from "@chakra-ui/react"
+
+const Login = () => {
+    return (
+        <VStack
+            as="form"
+            w={{ base: "90%", md: "500px" }}
+            m="auto"
+            justify="center"
+            h="100vh"
+            spacing="1rem"
+        >
+            <Heading>Log In</Heading>
+            <FormControl>
+                <FormLabel fontSize="lg">Username</FormLabel>
+                <Input name="username" placeholder="Enter username" autoComplete="off" size="lg"/>
+                <FormErrorMessage>Invalid username</FormErrorMessage>
+            </FormControl>
+
+            <FormControl>
+                <FormLabel fontSize="lg">Password</FormLabel>
+                <Input name="password" placeholder="Enter password" autoComplete="off" size="lg"/>
+                <FormErrorMessage>Invalid password</FormErrorMessage>
+            </FormControl>
+
+            <ButtonGroup pt="1rem">
+                <Button colorScheme="teal" type="submit">Log In</Button>
+                <Button>Create Account</Button>
+            </ButtonGroup>
+        </VStack>
+    )
+}
+
+export default Login;
--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,18 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from "./theme";
 import { ColorModeScript } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
